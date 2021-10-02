38cd014cad4fe151e6f82a1beec882e749252163
juan leon <juanjito.l2020@gmail.com>
2021-10-02 17:06:15 -0500

    creacion de  DTO ventas,productos,detalle ventas

---
+++ Tienda_libreria/src/main/java/modelo/ProductosDTO.java
@@ -0,0 +1,90 @@
+package modelo;
+
+public class ProductosDTO {
+	
+	private int codigo_producto;
+	private double ivacompra;
+	private int nitprovedor;
+	private String nombre_productos;
+	private double precio_compra;
+	private double precio_venta;
+	
+	
+	public ProductosDTO(int codigo_producto, double ivacompra, int nitprovedor, String nombre_productos,
+			double precio_compra, double precio_venta) {
+		
+		this.codigo_producto = codigo_producto;
+		this.ivacompra = ivacompra;
+		this.nitprovedor = nitprovedor;
+		this.nombre_productos = nombre_productos;
+		this.precio_compra = precio_compra;
+		this.precio_venta = precio_venta;
+	}
+
+
+	public int getCodigo_producto() {
+		return codigo_producto;
+	}
+
+
+	public void setCodigo_producto(int codigo_producto) {
+		this.codigo_producto = codigo_producto;
+	}
+
+
+	public double getIvacompra() {
+		return ivacompra;
+	}
+
+
+	public void setIvacompra(double ivacompra) {
+		this.ivacompra = ivacompra;
+	}
+
+
+	public int getNitprovedor() {
+		return nitprovedor;
+	}
+
+
+	public void setNitprovedor(int nitprovedor) {
+		this.nitprovedor = nitprovedor;
+	}
+
+
+	public String getNombre_productos() {
+		return nombre_productos;
+	}
+
+
+	public void setNombre_productos(String nombre_productos) {
+		this.nombre_productos = nombre_productos;
+	}
+
+
+	public double getPrecio_compra() {
+		return precio_compra;
+	}
+
+
+	public void setPrecio_compra(double precio_compra) {
+		this.precio_compra = precio_compra;
+	}
+
+
+	public double getPrecio_venta() {
+		return precio_venta;
+	}
+
+
+	public void setPrecio_venta(double precio_venta) {
+		this.precio_venta = precio_venta;
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ Tienda_libreria/src/main/java/modelo/ventas.java
@@ -0,0 +1,87 @@
+package modelo;
+
+public class ventas {
+	
+	private int codigo_venta;
+	private double ivaventa;
+	private double total_venta;
+	private double valor_venta;
+	private int cedula_clientes;
+	private int cedula_usuarios;
+	
+	
+	public ventas(int codigo_venta, double ivaventa, double total_venta, double valor_venta, int cedula_clientes,
+			int cedula_usuarios) {
+		super();
+		this.codigo_venta = codigo_venta;
+		this.ivaventa = ivaventa;
+		this.total_venta = total_venta;
+		this.valor_venta = valor_venta;
+		this.cedula_clientes = cedula_clientes;
+		this.cedula_usuarios = cedula_usuarios;
+	}
+
+
+	public int getCodigo_venta() {
+		return codigo_venta;
+	}
+
+
+	public void setCodigo_venta(int codigo_venta) {
+		this.codigo_venta = codigo_venta;
+	}
+
+
+	public double getIvaventa() {
+		return ivaventa;
+	}
+
+
+	public void setIvaventa(double ivaventa) {
+		this.ivaventa = ivaventa;
+	}
+
+
+	public double getTotal_venta() {
+		return total_venta;
+	}
+
+
+	public void setTotal_venta(double total_venta) {
+		this.total_venta = total_venta;
+	}
+
+
+	public double getValor_venta() {
+		return valor_venta;
+	}
+
+
+	public void setValor_venta(double valor_venta) {
+		this.valor_venta = valor_venta;
+	}
+
+
+	public int getCedula_clientes() {
+		return cedula_clientes;
+	}
+
+
+	public void setCedula_clientes(int cedula_clientes) {
+		this.cedula_clientes = cedula_clientes;
+	}
+
+
+	public int getCedula_usuarios() {
+		return cedula_usuarios;
+	}
+
+
+	public void setCedula_usuarios(int cedula_usuarios) {
+		this.cedula_usuarios = cedula_usuarios;
+	}
+	
+	
+	
+
+}
